beb5ebc45e3bd048a8c8007939fea9d1b2cba530
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2021-07-14 11:48:30 +0100

    added database

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const { v4: uuidv4, v4 } = require("uuid");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "ixnote",
+});
 
 var users = [];
 
@@ -18,7 +27,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.render("index", { users: users });
+  connection.query("SELECT * FROM students", (error, students) => {
+    if (!error) {
+      console.log(students);
+      res.render("index", { students: students });
+    } else {
+      console.log(error);
+    }
+  });
 });
 
 app.get("/register", (req, res) => {
@@ -26,25 +42,47 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/create_user", (req, res) => {
-  const id = v4();
-  const newuser = { id: id, ...req.body };
-  users.push(newuser);
-  res.redirect("/");
+  const { firstName, lastName, email, address } = req.body;
+
+  connection.query(
+    "INSERT INTO students (firstName, lastName, email, address) VALUES (?,?,?,?)",
+    [firstName, lastName, email, address],
+    (error, result) => {
+      if (!error) {
+        res.redirect("/");
+      } else {
+        console.log(error);
+      }
+    }
+  );
 });
 
 app.get("/remove/:id", (req, res) => {
   const id = req.params.id;
-  const deleteuser = users.filter((user) => user.id == id);
-  const index = users.indexOf(deleteuser[0]);
-  users.splice(index, 1);
-  console.log(users);
-  res.redirect("/");
+  connection.query("DELETE FROM students WHERE id=?", [id], (error, result) => {
+    if (!error) {
+      console.log(result);
+      res.redirect("/");
+    } else {
+      console.log(error);
+    }
+  });
 });
 
 app.get("/user/:id", (req, res) => {
   const id = req.params.id;
-  const user = users.filter((user) => user.id == id)[0];
-  res.render("user_detail", { user: user });
+  connection.query(
+    "SELECT * FROM students WHERE id=?",
+    [id],
+    (error, result) => {
+      if (!error) {
+        console.log(result);
+        res.render("user_detail", { student: result[0] });
+      } else {
+        console.log(error);
+      }
+    }
+  );
 });
 app.get("/edit/:id", (req, res) => {
   const id = req.params.id;
